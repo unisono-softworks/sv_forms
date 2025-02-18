98de75448714f6b3f7f448e3fc4d5054db1a69fa
Dennis Heiden <dennis-heiden@unisono.rocks>
2025-02-18 13:18:44 +0100

    update vnum

--- src/init.php
+++ src/init.php
@@ -6,7 +6,7 @@ if ( ! class_exists( '\sv_core\core_plugin' ) ) {
 }
 
 class init extends \sv_core\core_plugin {
-	const version 				= 2006;
+	const version 				= 2007;
 	const version_core_match 	= 10001;
 
     public function load() {
